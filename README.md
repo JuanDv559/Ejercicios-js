54f2e26acd9a17f3fdf020db962d95630beafd45
JuanDv559 <82281480+JuanDv559@users.noreply.github.com>
2024-07-11 07:59:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ejercicios-js
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/JuanDv559/Ejercicios-js)
---
+++ script.js
@@ -0,0 +1,162 @@
+alert(" 1. Realizar un programa que dado 2 números imprima por consola si el primer numero es mayor que el segundo. ");
+
+
+
+
+
+let numero1 = prompt("ingrese el primer numero");
+
+
+
+
+let numero2 = prompt("ingrese el segundo numero");
+
+
+
+let numero3 = undefined
+
+
+
+if(numero1>numero2){
+
+
+
+console.log("el primer numero es mayor que el segundo ");
+
+
+alert("revise la consola ");
+
+
+
+}else{ alert("el primer numero no es mayor que el segundo ")};
+
+
+
+
+
+
+
+
+numero1 = null;
+
+
+
+numero2 = null;
+
+
+
+
+alert("2. Realizar un programa que dado 2 números imprima por consola si los numeros son iguales o si son diferentes.")
+
+
+
+
+numero1 = prompt("ingrese el primer numero");
+
+
+
+numero2 = prompt("ingrese el segundo numero");
+
+
+
+
+if(numero1 == numero2){console.log("son iguales ")
+
+
+
+}else{console.log(" son diferentes")};
+
+
+
+
+alert("revise la consola");
+
+
+
+
+numero1=null;
+
+
+
+número2=null;
+
+
+
+
+alert("3.Realizar un programa que dado 2 números imprima por consola cual de los 2 numeros es el mas grande o si son iguales.");
+
+
+
+
+numero1 = prompt("ingrese el primer numero");
+
+
+
+numero2 = prompt("ingrese el segundo numero");
+
+
+
+
+if(numero1 > numero2){
+
+
+
+console.log(numero1 +" es mayor que "+numero2)
+
+
+
+} 
+
+
+
+if(numero1 == numero2){
+
+
+
+console.log(numero1+" es igual a "+numero2)
+
+
+
+
+}if (numero2<numero1) 
+
+
+{console.log(numero1+" es mayor que "+numero2)};
+
+
+alert("revise la consola");
+
+
+
+numero1=null;
+
+
+numero2=null;
+
+
+
+alert("4. Realizar un programa que dado 3 números imprima por pantalla cual de los 3 es el más chico.");
+
+
+
+numero1 = prompt("ingrese el primer numero");
+
+
+
+numero2 = prompt("ingrese el segundo numero");
+
+
+numero3 = prompt("ingrese el tercer numero");
+
+
+if(numero1<numero2&&numero1<numero3){
+
+console.log(numero1+" es menor que el resto")
+
+};
+
+if(numero2<numero1&&numero2<numero3){console.log(numero2+" es menor que el resto")};
+
+if(numero3<numero1&&numero3<numero2){console.log(numero3+" es menor que el resto")}
+
+
